fc9c0968cfc84b129a73eeb5160e733db9f9f74f
68681395 <happycoder@foxmail.com>
2020-11-18 11:57:41 +0800

    test bash

--- syncing.sh
+++ syncing.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
 
+read  -p 'Commit message>' msg
+ if [ -z "$msg" ]; then     
+	 msg="Synchronizing"
+ fi
+ 
+echo "Committing as '$msg'"
 git pull
 git add *
-git commit -m "auto commit by sh script"
+git add -A
+git commit -m "$msg"
 git push -u origin master
